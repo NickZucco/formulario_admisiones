1231e58f0fcbf4be0d55b7f3abe448c804fd65f7
Nicolas <nicastroco@unal.edu.co>
2017-03-08 23:31:24 -0500

    Se borraron modelos que no se usaban para nada

--- app/Configuracion.php
+++ app/Configuracion.php
@@ -4,8 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Configuracion extends Model
-{
+class Configuracion extends Model {
     protected $table = 'configuraciones';
     protected $filltable = array('*');
     protected $guarded = array('_token');
