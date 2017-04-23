f895476fec3e38c42625a8668077d2318c2aacbc
Mesi Rendon <amrendonsa@unal.edu.co>
2017-04-23 00:41:25 -0500

    Finalizando formulario desde aspirante

--- app/Http/Controllers/ReferenciasController.php
+++ app/Http/Controllers/ReferenciasController.php
@@ -8,12 +8,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Input;
 use Auth;
 use DB;
 use App\Pais as Pais;
 use App\Aspirante as Aspirante;
 use App\Referencia as Referencia;
+use App\AspiranteReferencia as AspiranteReferencia;
+use App\ProgramaPosgrado as ProgramaPosgrado;
 
 class ReferenciasController extends Controller
 {
@@ -26,47 +29,150 @@ class ReferenciasController extends Controller
             $programa_seleccionado = $main_data[1];
             $correo_area = $main_data[2];
             $paises = Pais::orderBy('nombre')->get();
-        }
-        catch (ErrorException $e) {
+            $aspirante_referencia = array();
+            foreach (AspiranteReferencia::where('aspirantes_id', '=', $aspirante_id)->get() as $ar) {
+                $ref_builder = array();
+                $ref_builder['ref'] = Referencia::where('id', '=', $ar->referencias_id)->first();
+                $ref_builder['dato'] = $ar;
+                array_push($aspirante_referencia, $ref_builder);
+            }
+            $opcion = null;
+            switch (ProgramaPosgrado::join('aspirantes', 'aspirantes.programa_posgrado_id', '=', 'programa_posgrado.id')
+                ->select('programa_posgrado.id')
+                ->where('aspirantes.id', '=', $aspirante_id)
+                ->first()->id) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 12:
+                case 14:
+                case 16:
+                case 18:
+                case 20:
+                case 22:
+                case 24:
+                case 26:
+                case 28:
+                case 30:
+                case 32:
+                case 33:
+                case 35:
+                case 37:
+                case 39:
+                    $opcion = 1;
+                    break;
+                case 13:
+                case 15:
+                case 17:
+                case 19:
+                case 21:
+                case 23:
+                case 25:
+                case 27:
+                case 29:
+                case 31:
+                case 34:
+                case 36:
+                case 38:
+                case 40:
+                    $opcion = 2;
+                    break;
+                default:
+                    $opcion = 0;
+            }
+        } catch (ErrorException $e) {
             $msg = "Error";
-        }
-        finally {
+        } finally {
             $data = array(
                 'id' => $aspirante_id,
                 'msg' => $msg,
                 'programa_seleccionado' => $programa_seleccionado,
                 'paises' => $paises,
                 'correo_area' => $correo_area,
-                'count' => $count
+                'count' => $count,
+                'referencias' => $aspirante_referencia,
+                'opcion' => $opcion
             );
             return view('formulario_referencias', $data);
         }
     }
 
-    public function save_references() {
+    public function save_references()
+    {
         $input = Input::all();
 
-        $aspirante = Aspirante::find(Auth::user()->id);
+        if(isset($input['delete1'])) {
+            AspiranteReferencia::destroy($input['delete1']);
+            return redirect('formulario_referencias');
+        }
 
-        $referencia1 = new Referencia;
-        $referencia1->nombre_de_referencia = $input['nombreApellido1'];
-        $referencia1->correo_de_referencia = $input['correo1'];
+        if(isset($input['delete2'])) {
+            AspiranteReferencia::destroy($input['delete2']);
+            return redirect('formulario_referencias');
+        }
 
-        $referencia2 = new Referencia;
-        $referencia2->nombre_de_referencia = $input['nombreApellido2'];
-        $referencia2->correo_de_referencia = $input['correo2'];
+        $referencia1 = null;
+        if ($input['correo1'] != '' && $input['nombreApellido1'] != '') {
+            $referencia1 = array_key_exists('currentCorreo1', $input) ? Referencia::where('correo_de_referencia', '=', $input['currentCorreo1'])->first() : new Referencia;
+            $referencia1->nombre_de_referencia = $input['nombreApellido1'];
+            $referencia1->correo_de_referencia = $input['correo1'];
+            try {
+                $referencia1->save();
+            } catch (QueryException $e) {
+                $referencia1 = Referencia::where('correo_de_referencia', '=', $input['correo1'])->first();
+            }
+        }
 
-        $referencia1->save();
-		$referencia2->save();
-        //var_dump($referencia1);
+        $referencia2 = null;
+        if ($input['correo2'] != '' && $input['nombreApellido2'] != '') {
+            $referencia2 = array_key_exists('currentCorreo2', $input) ? Referencia::where('correo_de_referencia', '=', $input['currentCorreo2'])->first() : new Referencia;
+            $referencia2->nombre_de_referencia = $input['nombreApellido2'];
+            $referencia2->correo_de_referencia = $input['correo2'];
+            try {
+                $referencia2->save();
+            } catch (QueryException $e) {
+                $referencia2 = Referencia::where('correo_de_referencia', '=', $input['correo2'])->first();
+            }
+        }
 
+        if (is_null($referencia1) && is_null($referencia2))
+            return redirect('formulario_referencias');
 
-//        $data = array(
-//            'nombreApellido1' => $referencia1->nombre_de_referencia,
-//            'correo1' => $referencia1->correo_de_referencia,
-//            'nombreApellido2' => $referencia2->nombre_de_referencia,
-//            'correo2' => $referencia2->correo_de_referencia..
-//        );
-//        return view('formulario_referencias', $data);
+        $refs = array();
+        if (!is_null($referencia1)) array_push($refs, $referencia1);
+        if (!is_null($referencia2)) array_push($refs, $referencia2);
+
+        $current_references = array();
+        foreach (AspiranteReferencia::where('aspirantes_id', '=', Auth::user()->id)
+                     ->whereIn('referencias_id', $refs)
+                     ->get() as $ar) {
+            array_push($current_references, $ar);
+        }
+
+        if (!is_null($referencia1)) {
+            $aspirante_referencia_1 = count($current_references) ? array_shift($current_references) : new AspiranteReferencia;
+            $aspirante_referencia_1->aspirantes_id = Auth::user()->id;
+            $aspirante_referencia_1->referencias_id = $referencia1->id;
+            if (array_key_exists('tipo1', $input)) {
+                $aspirante_referencia_1->tipo_referencia = $input['tipo1'];
+            }
+            $aspirante_referencia_1->save();
+        }
+
+        if (!is_null($referencia2)) {
+            $aspirante_referencia_2 = count($current_references) ? array_shift($current_references) : new AspiranteReferencia;
+            $aspirante_referencia_2->aspirantes_id = Auth::user()->id;
+            $aspirante_referencia_2->referencias_id = $referencia2->id;
+            if (array_key_exists('tipo2', $input)) {
+                $aspirante_referencia_2->tipo_referencia = $input['tipo2'];
+            }
+            $aspirante_referencia_2->save();
+        }
+        return redirect('formulario_referencias');
     }
 }
--- resources/views/formulario_referencias.blade.php
+++ resources/views/formulario_referencias.blade.php
@@ -1,105 +1,173 @@
 @extends('main')
 
 @section('form')
-    <div class="alert alert-warning alert-dismissible" role="alert">
-        <i class="fa fa-exclamation-circle" aria-hidden="true"></i> <strong>Nota: </strong>Por favor, tenga en cuenta
-        que toda la información que digite en el formulario, debe registrarse únicamente en español
-    </div>
-
-    <div class="panel panel-default">
-        @if(session()->has('message'))
-            <div class="alert alert-danger" role="alert">
-                {{ session()->get('message') }}
-            </div>
-        @endif
-
-        <div class="panel-heading">
-            <strong>Datos de quien va a dar la referencia</strong>
+    @if($opcion)
+        <div class="alert alert-warning alert-dismissible" role="alert">
+            <i class="fa fa-exclamation-circle" aria-hidden="true"></i> <strong>Nota: </strong>Por favor, tenga en
+            cuenta que toda la información que digite en el formulario, debe registrarse únicamente en español
         </div>
-        <form method="post" action="{{ env('APP_URL') }}formulario_referencias" class="form-horizontal" style="margin:20px 0"
-              enctype="multipart/form-data">
 
-            {!! csrf_field() !!}
-
-            <div class="panel-body">
-                <div class="form-group">
-                    <label for="nombreApellido1" class="col-sm-2 control-label">Nombres y Apellidos</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="nombreApellido1" name="nombreApellido1"
-                               placeholder="Nombres y Apellidos" required/>
-                    </div>
+        <div class="panel panel-default">
+            @if(session()->has('message'))
+                <div class="alert alert-danger" role="alert">
+                    {{ session()->get('message') }}
                 </div>
-                <div class="form-group">
-                    <label for="correo1" class="col-sm-2 control-label">Correo electrónico</label>
-                    <div class="col-sm-4">
-                        <input type="email" class="form-control" id="correo1" name="correo1"
-                               placeholder="ejemplo@proveedor.com" required/>
-                    </div>
-                    <label class="control-label col-md-2">Tipo de referencia</label>
-                    <div class="col-sm-4">
-                        <div class="radio">
-                            <label for="academica1">
-                                <input type="radio" name="tipo1" id="academica1" value="academica">
-                                Académica
-                            </label>
-                        </div>
-                        <div class="radio">
-                            <label for="personal1">
-                                <input type="radio" name="tipo1" id="personal1" value="personal">
-                                Profesional
-                            </label>
-                        </div>
-                    </div>
-                </div>
+            @endif
+            <div class="panel-heading">
+                <strong>Datos de quien va a dar la referencia{{$opcion == 1 ? ' académica.' : '.'}}</strong>
             </div>
-            <hr>
-            <div class="panel-body">
-                <div class="form-group">
-                    <label for="nombreApellido2" class="col-sm-2 control-label">Nombres y Apellidos</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="nombreApellido2" name="nombreApellido2"
-                               placeholder="Nombres y Apellidos" required/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="correo2" class="col-sm-2 control-label">Correo electrónico</label>
-                    <div class="col-sm-4">
-                        <input type="email" class="form-control" id="correo2" name="correo2"
-                               placeholder="ejemplo@proveedor.com" required/>
-                    </div>
-                    <label class="control-label col-md-2">Tipo de referencia</label>
-                    <div class="col-sm-4">
-                        <div class="radio">
-                            <label for="academica2">
-                                <input type="radio" name="tipo2" id="academica2" value="academica">
-                                Académica
-                            </label>
-                        </div>
-                        <div class="radio">
-                            <label for="personal2">
-                                <input type="radio" name="tipo2" id="personal2" value="personal">
-                                Profesional
-                            </label>
+            <form method="post" action="{{ env('APP_URL') }}formulario_referencias" class="form-horizontal"
+                  style="margin:20px 0"
+                  enctype="multipart/form-data">
+
+                {!! csrf_field() !!}
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label for="nombreApellido1" class="col-sm-2 control-label">Nombres y Apellidos</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nombreApellido1" name="nombreApellido1"
+                                   placeholder="Nombres y Apellidos" 
+                                   @if(isset($referencias[0]['dato']->referencia_completa) && $referencias[0]['dato']->referencia_completa)
+                                   disabled
+                                   @endif
+                                   value="{{isset($referencias[0]['ref']->nombre_de_referencia)?$referencias[0]['ref']->nombre_de_referencia:''}}"/>
                         </div>
                     </div>
+                    <div class="form-group">
+                        <label for="correo1" class="col-sm-2 control-label">Correo electrónico</label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control" id="correo1" name="correo1"
+                                   placeholder="ejemplo@proveedor.com" 
+                                   @if(isset($referencias[0]['dato']->referencia_completa) && $referencias[0]['dato']->referencia_completa)
+                                   disabled
+                                   @endif
+                                   value="{{isset($referencias[0]['ref']->correo_de_referencia)?$referencias[0]['ref']->correo_de_referencia:''}}"/>
+                            @if(isset($referencias[0]['dato']->referencia_completa))
+                                <input type="hidden" class="form-control" id="correo" name="currentCorreo1"
+                                       value="{{$referencias[0]['ref']->correo_de_referencia}}">
+                            @endif
+                        </div>
+                        @if($opcion == 1)
+                            <input type="hidden" name="tipo1" id="tipo1" value="0">
+                        @else
+                            <label class="control-label col-md-2">Tipo de referencia</label>
+                            <div class="col-sm-4">
+                                <div class="radio">
+                                    <label for="academica1">
+                                        <input type="radio" name="tipo1" id="academica1" value="0"
+                                               @if(isset($referencias[0]['dato']->tipo_referencia) && !$referencias[0]['dato']->tipo_referencia)
+                                               checked="checked"
+                                                @endif
+                                        >
+                                        Académica
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label for="personal1">
+                                        <input type="radio" name="tipo1" id="personal1" value="1"
+                                               @if(isset($referencias[0]['dato']->tipo_referencia) && $referencias[0]['dato']->tipo_referencia)
+                                               checked="checked"
+                                                @endif>
+                                        Profesional
+                                    </label>
+                                </div>
+                            </div>
+                        @endif
+                    </div>
+                    @if(isset($referencias[0]['dato']->referencia_completa) && !$referencias[0]['dato']->referencia_completa)
+                        <div class="form-group">
+                            <div class="col-sm-4 col-sm-offset-4">
+                                <button type="submit" class="btn btn-block btn-danger" id="delete1" name="delete1" value="{{$referencias[0]['dato']->id}}">
+                                    <i class="fa fa-user-times"></i>
+                                    Eliminar esta referencia
+                                </button>
+                            </div>
+                        </div>
+                    @endif
                 </div>
-            </div>
-
-            <input type="hidden" class="form-control" id="correo" name="correo">
-            <input type="hidden" class="form-control" id="id" name="id">
-
-            <div class="form-group">
-                <div class="col-md-4 col-md-offset-4">
-                    <button type="submit" class="btn btn-success form-control">
-                        <i class="fa fa-list-ul" aria-hidden="true"></i>
-                        <i class="fa fa-plus" aria-hidden="true"></i>
-                        Guardar referencias
-                    </button>
+                <hr>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label for="nombreApellido2" class="col-sm-2 control-label">Nombres y Apellidos</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nombreApellido2" name="nombreApellido2"
+                                   placeholder="Nombres y Apellidos" 
+                                   @if(isset($referencias[1]['dato']->referencia_completa) && $referencias[1]['dato']->referencia_completa)
+                                   disabled
+                                   @endif
+                                   value="{{isset($referencias[1]['ref']->nombre_de_referencia)?$referencias[1]['ref']->nombre_de_referencia:''}}"/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="correo2" class="col-sm-2 control-label">Correo electrónico</label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control" id="correo2" name="correo2"
+                                   placeholder="ejemplo@proveedor.com" 
+                                   @if(isset($referencias[1]['dato']->referencia_completa) && $referencias[1]['dato']->referencia_completa)
+                                   disabled
+                                   @endif
+                                   value="{{isset($referencias[1]['ref']->correo_de_referencia)?$referencias[1]['ref']->correo_de_referencia:''}}"/>
+                            @if(isset($referencias[1]['dato']->referencia_completa))
+                                <input type="hidden" class="form-control" id="correo" name="currentCorreo2"
+                                       value="{{$referencias[1]['ref']->correo_de_referencia}}">
+                            @endif
+                        </div>
+                        @if($opcion == 1)
+                            <input type="hidden" name="tipo2" id="tipo2" value="0">
+                        @else
+                            <label class="control-label col-md-2">Tipo de referencia</label>
+                            <div class="col-sm-4">
+                                <div class="radio">
+                                    <label for="academica2">
+                                        <input type="radio" name="tipo2" id="academica2" value="0"
+                                               @if(isset($referencias[1]['dato']->tipo_referencia) && !$referencias[1]['dato']->tipo_referencia)
+                                               checked="checked"
+                                                @endif
+                                        >
+                                        Académica
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label for="personal2">
+                                        <input type="radio" name="tipo2" id="personal2" value="1"
+                                               @if(isset($referencias[1]['dato']->tipo_referencia) && $referencias[1]['dato']->tipo_referencia)
+                                               checked="checked"
+                                                @endif
+                                        >
+                                        Profesional
+                                    </label>
+                                </div>
+                            </div>
+                        @endif
+                    </div>
+                    @if(isset($referencias[1]['dato']->referencia_completa) && !$referencias[1]['dato']->referencia_completa)
+                        <div class="form-group">
+                            <div class="col-sm-4 col-sm-offset-4">
+                                <button type="submit" class="btn btn-block btn-danger" id="delete2" name="delete2" value="{{$referencias[1]['dato']->id}}">
+                                    <i class="fa fa-user-times"></i>
+                                    Eliminar esta referencia
+                                </button>
+                            </div>
+                        </div>
+                    @endif
                 </div>
-            </div>
-        </form>
 
-    </div>
+                <input type="hidden" class="form-control" id="id" name="id">
+
+                <div class="form-group">
+                    <div class="col-md-4 col-md-offset-4">
+                        <button type="submit" class="btn btn-success form-control">
+                            <i class="fa fa-list-ul" aria-hidden="true"></i>
+                            <i class="fa fa-plus" aria-hidden="true"></i>
+                            Guardar referencias
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    @else
+        <h3 class="text-center">El programa al que aspira no solicita referencias</h3>
+    @endif
 
     <script>
 
